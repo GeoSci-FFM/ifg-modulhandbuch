379ef000be229cc803edd7013a73a60d49aa1cb2
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2025-07-23 12:59:44 +0200

    add impressum

--- pages/07_Impressum.py
+++ pages/07_Impressum.py
@@ -2,57 +2,56 @@ import streamlit as st
 
 st.title("Impressum")
 
-st.write('''Die Website wird als Diensteanbieterin im Rahmen des Kooperationsprojekts verantwortet von:
-Johann Wolfgang Goethe-Universität Frankfurt
-Theodor-W.-Adorno-Platz 1
-60323 Frankfurt am Main
+st.write('''Die Website wird als Diensteanbieterin im Rahmen des Kooperationsprojekts verantwortet von:  
+Johann Wolfgang Goethe-Universität Frankfurt  
+Theodor-W.-Adorno-Platz 1  
+60323 Frankfurt am Main  
 Deutschland
 
-Postanschrift:
-Goethe-Universität Frankfurt am Main
-60629 Frankfurt
-Deutschland
+Postanschrift:  
+Goethe-Universität Frankfurt am Main  
+60629 Frankfurt  
+Deutschland 
 
 Telefon: +49-69-798-0 | E-Mail:  webmaster@uni-frankfurt.de  
 
-Die Goethe-Universität Frankfurt am Main ist als Hochschule des Landes Hessen eine rechtsfähige Stiftung des öffentlichen Rechts (§ 88 HessHG).
+Die Goethe-Universität Frankfurt am Main ist als Hochschule des Landes Hessen eine rechtsfähige Stiftung des öffentlichen Rechts (§ 88 HessHG).  
 Umsatzsteuer-Identifikationsnummer (USt-IdNr.): DE 114 110 511
 
-Gesetzlicher Vertreter: 
+Gesetzlicher Vertreter:  
 Der Präsident der Goethe-Universität, Prof. Dr. Enrico Schleiff
 
-Verantwortliche/r Wissenschaftler/in, die/der diese Website koordiniert und für ihren Inhalt verantwortlich:
-NAME
-Fachbereich oder zuständiger Bereich
-an der Goethe-Universität Frankfurt
-Adresse Institut
-Kontakt-Email für dieses Projekt
-Tel.:
+Verantwortliche Person welche diese Website koordiniert:  
+Dominik Hezel  
+Institut für Geowissenschaften  
+an der Goethe-Universität Frankfurt  
+Adresse Institut  
+Kontakt-Email: dominik.hezel@em.uni-frankfurt.de  
+Kontakt-Telefon: +49 69 798 40122
 
-Zuständige Aufsichtsbehörde: 
+Zuständige Aufsichtsbehörde:  
 Hessisches Ministerium für Wissenschaft und Kunst, Rheinstr. 23-25, 65185 Wiesbaden.
-# [Für den Fall, dass Videos oder andere Medieninhalte eingebunden werden:
-# Zuständige Medienaufsichtsbehörde:
-# Medienanstalt Hessen, Wilhelmshöher Allee 262, 34131 Kassel]
 
-© COPYRIGHT
-Die Inhalte dieser Internet-Seiten sind urheberrechtlich geschützt. 
+© COPYRIGHT  
+Die Inhalte dieser Internet-Seiten sind urheberrechtlich geschützt.  
 Eine Nutzung des Logos, des Layouts und des HTML-Quelltextes sowie der Inhalte, Abbildungen und Fotos, auch in abgewandelter Form sowie die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung im gewerblichen Bereich bedürfen der schriftlichen Genehmigung durch die Vertretungsberechtigten der Kooperationspartner. Das Kopieren, der Download und/oder die Weiterverbreitung von Inhalten, auch in Auszügen, für pädagogische, wissenschaftliche oder private nicht-kommerzielle Zwecke ist unter Angabe der Quelle gestattet.  
 Beiträge Dritter sind als solche gekennzeichnet. Die Seitenbetreiber sind bemüht, stets die Urheberrechte anderer zu beachten bzw. auf selbst erstellte sowie lizenzfreie Werke zurückzugreifen.
 
-DISCLAIMER
-Haftungsbeschränkung für Inhalte der Website 
-Die Goethe-Universität als Diensteanbieterin übernimmt keine Haftung auf Aktualität, Richtigkeit und Vollständigkeit der zur Verfügung gestellten Inhalte oder Eignung für bestimmte Verwendungszwecke. Dies gilt nicht, wenn dem Verantwortlichen vorsätzliches oder grob fahrlässiges Verhalten vorzuwerfen ist. Die Inhalte werden von uns in der Regel elektronisch erstellt und manuell überarbeitet. Dennoch kann die inhaltliche Richtigkeit, insbesondere bei komplexen Themen nicht gewährleistet werden, so dass die Diensteanbieterin den Nutzern empfiehlt, bei wichtigen Informationen bei den zuständigen Stellen anzufragen oder rechtliche Beratung in Anspruch zu nehmen.
-Der Website-Betreiber ist für fremde Inhalte, die er für einen Nutzer veröffentlicht, nicht verantwortlich, sofern
+DISCLAIMER  
+Haftungsbeschränkung für Inhalte der Website  
+Die Goethe-Universität als Diensteanbieterin übernimmt keine Haftung auf Aktualität, Richtigkeit und Vollständigkeit der zur Verfügung gestellten Inhalte oder Eignung für bestimmte Verwendungszwecke. Dies gilt nicht, wenn dem Verantwortlichen vorsätzliches oder grob fahrlässiges Verhalten vorzuwerfen ist. Die Inhalte werden von uns in der Regel elektronisch erstellt und manuell überarbeitet. Dennoch kann die inhaltliche Richtigkeit, insbesondere bei komplexen Themen nicht gewährleistet werden, so dass die Diensteanbieterin den Nutzern empfiehlt, bei wichtigen Informationen bei den zuständigen Stellen anzufragen oder rechtliche Beratung in Anspruch zu nehmen.  
+Der Website-Betreiber ist für fremde Inhalte, die er für einen Nutzer veröffentlicht, nicht verantwortlich, sofern  
+         
 -	er keine Kenntnis von der rechtswidrigen Handlung oder der Information hat und ihm im Falle von Schadensersatzansprüchen auch keine Tatsachen oder Umstände bekannt sind, aus denen die rechtswidrige Handlung oder die Information offensichtlich wird, oder
 -	er unverzüglich tätig geworden ist, um die Information zu entfernen oder den Zugang zu ihr zu sperren, sobald er diese Kenntnis erlangt hat.
-Eine diesbezügliche Haftung ist somit erst ab dem Zeitpunkt der Kenntniserlangung einer konkreten Rechtsverletzung möglich.
+
+Eine diesbezügliche Haftung ist somit erst ab dem Zeitpunkt der Kenntniserlangung einer konkreten Rechtsverletzung möglich.  
 Erlangt die Goethe-Universität Kenntnis von solchen rechtswidrigen Inhalten, werden diese unverzüglich entfernt.
 
-Verlinkung auf fremde Webseiten
-Die Internet-Seiten enthalten Links zu externen Webseiten Dritter, auf deren Inhalte die Goethe-Universität keinen Einfluss hat. Sie macht sich ausdrücklich mit dem Setzen des Links auch nicht die hinter dem Link liegenden Inhalte zu Eigen. Daher kann keine Haftung für die Aktualität, Richtigkeit und Vollständigkeit der Inhalte der fremden Website übernommen werden. 
+Verlinkung auf fremde Webseiten  
+Die Internet-Seiten enthalten Links zu externen Webseiten Dritter, auf deren Inhalte die Goethe-Universität keinen Einfluss hat. Sie macht sich ausdrücklich mit dem Setzen des Links auch nicht die hinter dem Link liegenden Inhalte zu Eigen. Daher kann keine Haftung für die Aktualität, Richtigkeit und Vollständigkeit der Inhalte der fremden Website übernommen werden.  
 Für die Inhalte ist stets der jeweilige Anbieter der verlinkten Webseite verantwortlich. Zum Zeitpunkt des Setzens der Verlinkung wurde die fremde Website im Rahmen des Zumutbaren geprüft und es waren keinerlei rechtswidrigen Inhalte oder rechtliche Verstöße bekannt oder erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Erhält die Goethe-Universität Kenntnis von der Rechtswidrigkeit der verlinkten Inhalte, wird der entsprechende Link unverzüglich entfernt.
 
-Nutzung der Kontaktdaten
+Nutzung der Kontaktdaten  
 Der Nutzung von im Rahmen dieser Internetseiten veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien aber auch von rechtswidrigen Inhalten (Trojaner u.a.) wird hiermit ausdrücklich widersprochen. Die Goethe-Universität behält sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
-'''
+''')
